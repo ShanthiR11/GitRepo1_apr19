553e25355dc2c8b91aa621009c614969628b7458
ShanthiR11 <Shaab8987@gmail.com>
2025-04-19 10:04:38 +0530

    second try

--- src/test/java/org/test/Test.java
+++ src/test/java/org/test/Test.java
@@ -5,6 +5,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My First Project");
+		System.out.println("Add Commit Push");
+
 	}
 
 }
